f99dadc29bdd046e41be2dba97316ed31e80afaf
Andrew Morris <voltrevo@gmail.com>
2021-04-19 11:10:45 +1000

    Hello world transcription works (got 'hollo werld' but still)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import * as fs from 'fs';
+
 import * as deepspeech from 'deepspeech';
 
 function totalTime(hrtimeValue: number[]): string {
   return (hrtimeValue[0] + hrtimeValue[1] / 1000000000).toPrecision(4);
 }
 
-const modelPath = `${process.env.HOME}/data/deepspeech-exp/deepspeech-0.9.3-models.pbmm`;
+const dataDir = `${process.env.HOME}/data/deepspeech-exp`;
+
+const modelPath = `${dataDir}/deepspeech-0.9.3-models.pbmm`;
 
 console.error('Loading model from file %s', modelPath);
 const modelLoadStart = process.hrtime();
@@ -15,3 +19,10 @@ console.error('Loaded model in %ds.', totalTime(modelLoadEnd));
 // if (args.beam_width) {
 //   model.setBeamWidth(args.beam_width);
 // }
+
+const output = model.stt(
+  fs.readFileSync(`${dataDir}/hello-world.wav`),
+);
+
+console.log(output);
+// console.log(output.transcripts.map(tr => tr.tokens));
