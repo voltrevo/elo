36daec9389c90c4dbbc6a20c4d1d8f3c85deb995
Andrew Morris <voltrevo@gmail.com>
2021-04-19 10:42:55 +1000

    Got model loading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import * as deepspeech from 'deepspeech';
+
+function totalTime(hrtimeValue: number[]): string {
+  return (hrtimeValue[0] + hrtimeValue[1] / 1000000000).toPrecision(4);
+}
+
+const modelPath = `${process.env.HOME}/data/deepspeech-exp/deepspeech-0.9.3-models.pbmm`;
+
+console.error('Loading model from file %s', modelPath);
+const modelLoadStart = process.hrtime();
+let model = new deepspeech.Model(modelPath);
+const modelLoadEnd = process.hrtime(modelLoadStart);
+console.error('Loaded model in %ds.', totalTime(modelLoadEnd));
+
+// if (args.beam_width) {
+//   model.setBeamWidth(args.beam_width);
+// }
