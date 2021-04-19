1a973f63995364fbcd1642900cfc5d7691635f16
Andrew Morris <voltrevo@gmail.com>
2021-04-19 13:06:48 +1000

    Basic record and playback, seems to be capturing silence though

--- src/web-frontends/recorder/index.tsx
+++ src/web-frontends/recorder/index.tsx
@@ -1,5 +1,11 @@
 import * as preact from 'preact';
+import playAudio from './playAudio';
+
+import recordAudio from './recordAudio';
 
 window.addEventListener('load', () => {
   preact.render(<div>Hello world!</div>, document.body);
 });
+
+(window as any).recordAudio = recordAudio;
+(window as any).playAudio = playAudio;
