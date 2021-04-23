870d5fcea6bcb8b15747f10b2781830109e200bc
Andrew Morris <voltrevo@gmail.com>
2021-04-23 12:12:23 +1000

    Fix token alignment bug

--- src/analyze.ts
+++ src/analyze.ts
@@ -1,15 +1,14 @@
 import * as fs from 'fs';
 import { Readable as ReadableStream } from 'stream';
 
-import * as diff from 'diff';
 import * as deepspeech from 'deepspeech';
 import ffmpeg from 'ffmpeg';
 import { streamToBuffer } from '@jorgeferrero/stream-to-buffer';
 import * as musicMetadata from 'music-metadata';
 
 import dirs from './dirs';
-import assert from './helpers/assert';
 import assertExists from './helpers/assertExists';
+import analyzeTargetTranscript, { TargetAnalysis } from './analyzeTargetTranscript';
 
 function totalTime(hrtimeValue: number[]): string {
   return (hrtimeValue[0] + hrtimeValue[1] / 1000000000).toPrecision(4);
@@ -22,12 +21,6 @@ const modelLoadStart = process.hrtime();
 const model = new deepspeech.Model(modelPath);
 const modelLoadEnd = process.hrtime(modelLoadStart);
 console.error('Loaded model in %ds.', totalTime(modelLoadEnd));
-
-type TargetAnalysis = {
-  targetTranscript: string,
-  speechTranscript: string,
-  tokens: AnalysisToken[],
-};
 
 export type Analysis = {
   deepspeech: deepspeech.Metadata,
@@ -59,80 +52,6 @@ export default async function analyze(
   }
 
   return analysis;
-}
-
-export function analyzeTargetTranscript(
-  deepspeechAnalysis: Analysis['deepspeech'],
-  targetTranscript: string,
-): TargetAnalysis {
-  const originalDeepspeechTokens = deepspeechAnalysis.transcripts[0].tokens;
-  const originalDeepspeechTranscript = originalDeepspeechTokens.map(t => t.text).join('');
-
-  const deepspeechTokens = originalDeepspeechTokens.filter(t => t.text !== '\'');
-  const deepspeechTranscript = deepspeechTokens.map(t => t.text).join('');
-
-  const rawTargetTranscriptWithCase = targetTranscript.replace(/[^a-zA-Z ]/g, '');
-  const rawTargetTranscript = rawTargetTranscriptWithCase.toLowerCase();
-
-  const hunks = diff.diffChars(rawTargetTranscript, deepspeechTranscript);
-
-  const tokens: AnalysisToken[] = [];
-  let tokenPos = 0;
-  let rawTargetTranscriptWithCasePos = 0;
-  let targetTranscriptPos = 0;
-
-  function syncPunctuation() {
-    while (
-      targetTranscriptPos < targetTranscript.length &&
-      targetTranscript[targetTranscriptPos] !==
-      rawTargetTranscriptWithCase[rawTargetTranscriptWithCasePos]
-    ) {
-      tokens.push({
-        text: targetTranscript[targetTranscriptPos],
-      });
-
-      targetTranscriptPos++;
-    }
-  }
-
-  for (const hunk of hunks) {
-    for (const c of hunk.value) {
-      syncPunctuation();
-      const correct = !hunk.added && !hunk.removed;
-
-      if (!hunk.removed) {
-        tokens.push({
-          ...deepspeechAnalysis.transcripts[0].tokens[tokenPos],
-          text: hunk.added
-            ? deepspeechAnalysis.transcripts[0].tokens[tokenPos].text
-            : rawTargetTranscriptWithCase[rawTargetTranscriptWithCasePos],
-          type: correct ? 'correct' : 'spoken-incorrect',
-        });
-
-        tokenPos++;
-      } else {
-        tokens.push({
-          text: c,
-          type: 'missed',
-        });
-      }
-
-      if (!hunk.added) {
-        rawTargetTranscriptWithCasePos++;
-        targetTranscriptPos++;
-      }
-    }
-  }
-
-  syncPunctuation();
-
-  assert(tokenPos === deepspeechTranscript.length);
-
-  return {
-    targetTranscript,
-    speechTranscript: originalDeepspeechTranscript,
-    tokens,
-  };
 }
 
 async function getWavFile(webmStream: ReadableStream) {
---
+++ src/analyzeTargetTranscript.ts
@@ -0,0 +1,85 @@
+import type * as deepspeech from 'deepspeech';
+import * as diff from 'diff';
+
+import assert from './helpers/assert';
+import type { AnalysisToken } from './analyze';
+
+export type TargetAnalysis = {
+  targetTranscript: string,
+  speechTranscript: string,
+  tokens: AnalysisToken[],
+};
+
+export default function analyzeTargetTranscript(
+  deepspeechAnalysis: deepspeech.Metadata,
+  targetTranscript: string,
+): TargetAnalysis {
+  const originalDeepspeechTokens = deepspeechAnalysis.transcripts[0].tokens;
+  const originalDeepspeechTranscript = originalDeepspeechTokens.map(t => t.text).join('');
+
+  const deepspeechTokens = originalDeepspeechTokens.filter(t => t.text !== '\'');
+  const deepspeechTranscript = deepspeechTokens.map(t => t.text).join('');
+
+  const rawTargetTranscriptWithCase = targetTranscript.replace(/[^a-zA-Z ]/g, '');
+  const rawTargetTranscript = rawTargetTranscriptWithCase.toLowerCase();
+
+  const hunks = diff.diffChars(rawTargetTranscript, deepspeechTranscript);
+
+  const tokens: AnalysisToken[] = [];
+  let tokenPos = 0;
+  let rawTargetTranscriptWithCasePos = 0;
+  let targetTranscriptPos = 0;
+
+  function syncPunctuation() {
+    while (
+      targetTranscriptPos < targetTranscript.length &&
+      targetTranscript[targetTranscriptPos] !==
+      rawTargetTranscriptWithCase[rawTargetTranscriptWithCasePos]
+    ) {
+      tokens.push({
+        text: targetTranscript[targetTranscriptPos],
+      });
+
+      targetTranscriptPos++;
+    }
+  }
+
+  for (const hunk of hunks) {
+    for (const c of hunk.value) {
+      syncPunctuation();
+      const correct = !hunk.added && !hunk.removed;
+
+      if (!hunk.removed) {
+        tokens.push({
+          ...deepspeechTokens[tokenPos],
+          text: hunk.added
+            ? deepspeechTokens[tokenPos].text
+            : rawTargetTranscriptWithCase[rawTargetTranscriptWithCasePos],
+          type: correct ? 'correct' : 'spoken-incorrect',
+        });
+
+        tokenPos++;
+      } else {
+        tokens.push({
+          text: c,
+          type: 'missed',
+        });
+      }
+
+      if (!hunk.added) {
+        rawTargetTranscriptWithCasePos++;
+        targetTranscriptPos++;
+      }
+    }
+  }
+
+  syncPunctuation();
+
+  assert(tokenPos === deepspeechTranscript.length);
+
+  return {
+    targetTranscript,
+    speechTranscript: originalDeepspeechTranscript,
+    tokens,
+  };
+}
