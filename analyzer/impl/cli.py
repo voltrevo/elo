a30790cb27b1f89b76ae19999cdf45cb5dd4c25b
Andrew Morris <voltrevo@gmail.com>
2021-05-03 05:09:36 +0000

    Pass target_transcript using argparse

--- analyzer/impl/cli.py
+++ analyzer/impl/cli.py
@@ -1,9 +1,16 @@
+import argparse
 import sys
 
 from .analyze import analyze
 
 def run():
+  parser = argparse.ArgumentParser(description='Fluency CLI')
+
+  parser.add_argument('--target_transcript')
+
+  args = parser.parse_args()
+
   bytes = sys.stdin.buffer.read(50000000)
-  result = analyze(bytes)
+  result = analyze(bytes, args.target_transcript)
 
   print(result)
