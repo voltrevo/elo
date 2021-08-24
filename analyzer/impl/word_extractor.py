241b873715bdbec19cbce65cac04f9538446ff56
Andrew Morris <voltrevo@gmail.com>
2021-08-24 06:43:32 +0000

    Add pause detection to WordExtractor

--- analyzer/impl/word_extractor.py
+++ analyzer/impl/word_extractor.py
@@ -3,20 +3,23 @@ from typing import Callable, List, Optional
 from .types import AnalysisToken, AnalysisWord
 
 
+pause_threshold = 0.8
+
 class WordExtractor:
   partial_word: List[AnalysisToken] = []
   space_before: Optional[AnalysisToken] = None
+  last_end_time: Optional[float] = None
 
   def __init__(self, on_word: Callable[[AnalysisWord], None]):
     self.on_word = on_word
-  
+
   def process_token(self, token: AnalysisToken):
     if token.text == ' ':
       self.append_word(token)
       self.space_before = token
     else:
       self.partial_word.append(token)
-  
+
   def end(self):
     self.append_word(None)
 
@@ -39,14 +42,32 @@ class WordExtractor:
 
       if start_time is None:
         start_time = t.start_time
-      
+
       end_time = t.start_time
-    
+
+    if (
+      self.last_end_time is not None and
+      start_time is not None and
+      start_time - self.last_end_time >= pause_threshold
+    ):
+      self.on_word(AnalysisWord(
+        start_time=self.last_end_time + 0.05,
+        end_time=start_time - 0.05,
+        disfluent=True,
+        text="<pause>",
+      ))
+
+    text = ''.join(['' if t.text is None else t.text for t in self.partial_word])
+
+    if text == '':
+      text = '<?>'
+
     self.on_word(AnalysisWord(
       start_time=start_time,
       end_time=end_time,
       disfluent=is_disfluent,
-      text=''.join(['' if t.text is None else t.text for t in self.partial_word])
+      text=text,
     ))
 
+    self.last_end_time = end_time
     self.partial_word.clear()
