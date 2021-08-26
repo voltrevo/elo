d40ad719250fc04fede4b2a0c145e340447ff30a
Andrew Morris <voltrevo@gmail.com>
2021-08-26 01:58:13 +0000

    Get inside page

--- src/web-frontends/extension/contentScript.tsx
+++ src/web-frontends/extension/contentScript.tsx
@@ -1,1 +1,9 @@
-console.log('Hello (content script)');
+function pageScript() {
+  console.log('Hello inside page');
+  (window as any).foo = 'baz';
+}
+
+const pageScriptTag = document.createElement('script');
+pageScriptTag.textContent = `(${pageScript.toString()})();`;
+
+document.body.appendChild(pageScriptTag);
