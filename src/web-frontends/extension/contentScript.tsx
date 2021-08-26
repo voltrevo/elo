453853b1b049bc4626ea6f84739915d127414a13
Andrew Morris <voltrevo@gmail.com>
2021-08-26 02:50:55 +0000

    Detect getUserMedia, be the first script to run inside the page

--- src/web-frontends/extension/contentScript.tsx
+++ src/web-frontends/extension/contentScript.tsx
@@ -1,9 +1,26 @@
 function pageScript() {
-  console.log('Hello inside page');
-  (window as any).foo = 'baz';
+  // new
+  (() => {
+    const originalGum = navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices);
+
+    navigator.mediaDevices.getUserMedia = (...args) => {
+      console.log('getUserMedia detected', args);
+      return originalGum(...args);
+    };
+  })();
+
+  // old
+  (() => {
+    const originalGum = navigator.getUserMedia.bind(navigator);
+
+    navigator.getUserMedia = (...args) => {
+      console.log('old getUserMedia detected', args);
+      return originalGum(...args);
+    };
+  })();
 }
 
 const pageScriptTag = document.createElement('script');
 pageScriptTag.textContent = `(${pageScript.toString()})();`;
 
-document.body.appendChild(pageScriptTag);
+document.documentElement.appendChild(pageScriptTag);
