84a5bb8b6ed7c5fdd6b7033ee326a95cf9cc0f0f
Andrew Morris <voltrevo@gmail.com>
2021-08-26 04:47:44 +0000

    Basic layout

--- src/web-frontends/extension/components/App.tsx
+++ src/web-frontends/extension/components/App.tsx
@@ -2,6 +2,15 @@ import * as preact from 'preact';
 
 export default class App extends preact.Component<{}, {}> {
   render(): preact.ComponentChild {
-    return <div class="app">Hello world</div>;
+    this;
+
+    return <div class="app">
+      <div class="head">Fluency Extension</div>
+      <div class="body">
+        <div class="word">
+          Test
+        </div>
+      </div>
+    </div>;
   }
 }
