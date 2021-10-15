93301c8706135c31d140dde7a5bf2ee43b2af7b0
Andrew Morris <voltrevo@gmail.com>
2021-10-15 06:30:17 +0000

    DropDetector

---
+++ src/web-frontends/labelling-tool/DropDetector.tsx
@@ -0,0 +1,60 @@
+import * as preact from 'preact';
+
+type Props = {
+  onDrop: (file: File) => void;
+};
+
+export default class DropDetector extends preact.Component<Props> {
+  render() {
+    return <div
+      class="drop-detector"
+      style={{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        zIndex: 1,
+      }}
+      onDrop={async (event) => {
+        event.preventDefault();
+
+        const target = event.target as HTMLElement;
+
+        if (!target.classList.contains('drop-detector')) {
+          return;
+        }
+
+        target.classList.remove('drop-me');
+
+        const dropFiles = event.dataTransfer?.files;
+
+        if (dropFiles === undefined) {
+          return;
+        }
+
+        for (let i = 0; i < dropFiles.length; i++) {
+          const file = dropFiles.item(i);
+
+          if (file === null) {
+            continue;
+          }
+
+          this.props.onDrop(file);
+          break; // TODO: Multiple files?
+        }
+      }}
+      onDragOver={(event) => event.preventDefault()}
+      onDragEnter={(event) => {
+        if (event.target instanceof HTMLElement && event.target.classList.contains('drop-detector')) {
+          event.target.classList.add('drop-me');
+        }
+      }}
+      onDragLeave={(event) => {
+        if (event.target instanceof HTMLElement && event.target.classList.contains('drop-detector')) {
+          event.target.classList.remove('drop-me');
+        }
+      }}
+    ></div>;
+  }
+}
--- src/web-frontends/labelling-tool/LabellingTool.tsx
+++ src/web-frontends/labelling-tool/LabellingTool.tsx
@@ -1,9 +1,18 @@
 import * as preact from 'preact';
 
+import DropDetector from './DropDetector';
+
 export default class LabellingTool extends preact.Component {
   render() {
     this;
 
-    return <>Labelling Tool</>;
+    return <>
+      Labelling Tool
+
+      <div style={{ position: 'relative', width: '100px', height: '100px' }}>
+        <DropDetector onDrop={(f) => console.log('drop detected', f)}/>
+        Test
+      </div>
+    </>;
   }
 }
