a7b981863db85ab5baf6b3e7769ab3f625e19e99
Andrew Morris <voltrevo@gmail.com>
2021-11-10 15:13:57 +1100

    wip report prototype

--- src/web-frontends/extension/page/Page.tsx
+++ src/web-frontends/extension/page/Page.tsx
@@ -18,23 +18,67 @@ export default class Page extends preact.Component {
   render() {
     this;
     return <div class="elo-page">
-      <h1>Elo</h1>
+      <div class="elo-page-container">
+        <img src="/assets/icons/icon128.png" width="64" height="64" />
 
-      <div class="highlight-stat-container">
-        <div class="highlight-stat">
-          <div class="value">{(sampleData.totalDuration / 3600000).toFixed(1)}</div>
-          <div class="name">Hours Analyzed</div>
+        <div class="heading">
+          <div>
+            <div class="your-weekly-report">Your Weekly Report</div>
+            <div>Nov 8-14, 2021</div>
+            <div class="stats">
+
+              <table>
+                <thead></thead>
+                <tbody>
+                  <tr>
+                    <td>Sessions Logged</td>
+                    <td class="important-value numeric">12</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Speaking Time</td>
+                    <td class="important-value numeric">123</td>
+                    <td>min</td>
+                  </tr>
+                  <tr>
+                    <td>Total Time</td>
+                    <td class="important-value numeric">280</td>
+                    <td>min</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div>
+            <div class="user-label">User</div>
+            <div class="user-proper-name">Alexander Nick</div>
+          </div>
         </div>
-        <div class="highlight-stat">
-          <div class="value">{sampleData.totalSessions}</div>
-          <div class="name">Sessions</div>
+
+        <div class="single-stat-boxes">
+          <div>Combined</div>
+          <div>ums and uhs</div>
+          <div>Filler and Hedge Words</div>
         </div>
-      </div>
 
-      <div>
-        <h2>Recent Activity</h2>
+        <h1>Elo</h1>
+
+        <div class="highlight-stat-container">
+          <div class="highlight-stat">
+            <div class="value">{(sampleData.totalDuration / 3600000).toFixed(1)}</div>
+            <div class="name">Hours Analyzed</div>
+          </div>
+          <div class="highlight-stat">
+            <div class="value">{sampleData.totalSessions}</div>
+            <div class="name">Sessions</div>
+          </div>
+        </div>
+
+        <div>
+          <h2>Recent Activity</h2>
 
 
+        </div>
       </div>
     </div>;
   }
