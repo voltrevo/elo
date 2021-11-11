facf272f4e6a40e98cc7ad9a997b1fc0f42c1707
Andrew Morris <voltrevo@gmail.com>
2021-11-11 12:25:08 +1100

    ContentApp

--- src/web-frontends/extension/ContentApp.ts
+++ src/web-frontends/extension/ContentApp.ts
@@ -1,7 +1,15 @@
-import { AnalysisFragment } from "../../analyze";
+import { AnalysisDisfluent, AnalysisFragment } from "../../analyze";
+import never from "../../helpers/never";
 import TaskQueue from "../../helpers/TaskQueue";
+import EwmaCalculator from "../helpers/EwmaCalculator";
 import Protocol, { ConnectionEvent, PromisishApi } from "./Protocol";
 import UiState from "./UiState";
+
+type SessionStats = {
+  speakingTime: number;
+  totalTime: number;
+  featureCounts: Record<string, Record<string, number>>;
+};
 
 export default class ContentApp implements PromisishApi<Protocol> {
   uiState: UiState = {
@@ -21,16 +29,101 @@ export default class ContentApp implements PromisishApi<Protocol> {
     },
   };
 
+  sessionStats: SessionStats = {
+    speakingTime: 0,
+    totalTime: 0,
+    featureCounts: {},
+  };
+
   uiStateRequests = new TaskQueue();
+
+  fillerSoundEwma = new EwmaCalculator(60, 60);
+  fillerWordEwma = new EwmaCalculator(60, 60);
 
   updateUi() {
     this.uiState.index++;
     this.uiStateRequests.run();
   }
 
-  notifyGetUserMediaCalled() {}
-  addFragment(fragment: AnalysisFragment) {}
-  addConnectionEvent(evt: ConnectionEvent) {}
+  notifyGetUserMediaCalled() {
+    if (this.uiState.active) {
+      return;
+    }
+
+    this.uiState.active = true;
+    this.updateUi();
+  }
+
+  addFragment(fragment: AnalysisFragment) {
+    switch (fragment.type) {
+      case 'word': {
+        this.uiState.word = fragment.value.text;
+        this.updateUi();
+
+        break;
+      }
+
+      case 'disfluent': {
+        this.updateFeatureCount(fragment.value);
+
+        if (fragment.value.category === 'filler') {
+          this.fillerSoundEwma.observe(1);
+        } else {
+          this.fillerWordEwma.observe(1);
+        }
+
+        this.updateMetrics();
+
+        break;
+      }
+
+      case 'progress': {
+        this.fillerSoundEwma.timeDecay(fragment.value.speaking_time);
+        this.fillerWordEwma.timeDecay(fragment.value.speaking_time);
+        this.updateMetrics();
+
+        break;
+      }
+
+      case 'token':
+      case 'error':
+      case 'debug':
+      case 'end': {
+        break;
+      }
+
+      default: {
+        never(fragment);
+      }
+    }
+  }
+
+  addConnectionEvent(evt: ConnectionEvent) {
+    switch (evt) {
+      case 'connecting':
+      case 'reconnecting': {
+        if (!this.uiState.loading) {
+          this.uiState.loading = true;
+          this.updateUi();
+        }
+
+        break;
+      }
+
+      case 'connected': {
+        if (this.uiState.loading) {
+          this.uiState.loading = false;
+          this.updateUi();
+        }
+
+        break;
+      }
+
+      default: {
+        never(evt);
+      }
+    }
+  }
 
   getUiState(afterIndex: number) {
     if (this.uiState.index > afterIndex) {
@@ -40,5 +133,31 @@ export default class ContentApp implements PromisishApi<Protocol> {
     return new Promise<UiState>((resolve) => {
       this.uiStateRequests.push(() => resolve(this.uiState));
     });
+  }
+
+  updateFeatureCount(disfluent: AnalysisDisfluent) {
+    let category = this.sessionStats.featureCounts[disfluent.category];
+
+    if (category === undefined) {
+      category = {};
+      this.sessionStats.featureCounts[disfluent.category] = category;
+    }
+
+    category[disfluent.text] = (category[disfluent.text] ?? 0) + 1;
+  }
+
+  updateMetrics() {
+    const fillerSoundMetric = this.fillerSoundEwma.value.toFixed(1);
+    const fillerWordMetric = this.fillerWordEwma.value.toFixed(1);
+
+    if (
+      this.uiState.fillerSoundBox.metric !== fillerSoundMetric ||
+      this.uiState.fillerWordBox.metric !== fillerWordMetric
+    ) {
+      this.uiState.fillerSoundBox.metric = fillerSoundMetric;
+      this.uiState.fillerWordBox.metric = fillerWordMetric;
+
+      this.updateUi();
+    }
   }
 }
