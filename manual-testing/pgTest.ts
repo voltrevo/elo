6c554149d4da49b96b1e659a0ccec5a3348426ad
Andrew Morris <voltrevo@gmail.com>
2022-01-19 01:38:07 +0000

    Use inc methods in pgTest, move to manual-testing

---
+++ manual-testing/pgTest.ts
@@ -0,0 +1,25 @@
+import DbClient from '../src/database/DbClient';
+import config from '../src/helpers/config';
+import launch from '../src/helpers/launch';
+
+launch(async () => {
+  const db = await DbClient.connect(config.server.pgConnString);
+
+  await Promise.all([
+    db.incSession(),
+    db.incSpeakersPct(),
+    db.incSpeakersPct(),
+    db.incStreamsPct(),
+    db.incStreamsPct(),
+    db.incStreamsPct(),
+  ]);
+
+  const stats = await db.HourlyStats(
+    new Date(Date.now() - 86400000),
+    new Date(Date.now() + 3600000),
+  );
+
+  await db.disconnect();
+
+  return stats;
+});
