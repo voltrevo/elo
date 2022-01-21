601b7d25bdbc98599fa40ff9b46a1295c4c2efd1
Andrew Morris <voltrevo@gmail.com>
2022-01-21 02:51:10 +0000

    Split out ReconnectablePgClient

--- src/database/DbClient.ts
+++ src/database/DbClient.ts
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 
 import PgClient from './PgClient';
+import ReconnectablePgClient from './ReconnectablePgClient';
 
 export type HourlyStat = {
   hour: Date;
@@ -10,75 +11,18 @@ export type HourlyStat = {
 };
 
 export default class DbClient {
-  private pgClientState?: {
-    client: PgClient;
-    connected: boolean;
-    connectionPromise: Promise<void>;
-  };
+  reconnectablePgClient: ReconnectablePgClient;
 
-  constructor(private pgConnString: string) {}
+  constructor(pgConnString: string) {
+    this.reconnectablePgClient = new ReconnectablePgClient(pgConnString);
+  }
 
-  private async PgClient(): Promise<PgClient> {
-    if (this.pgClientState === undefined) {
-      const client = new PgClient(this.pgConnString);
-
-      const state: DbClient['pgClientState'] = {
-        client,
-        connected: false,
-        connectionPromise: client.connect(),
-      };
-
-      state.connectionPromise.then(
-        () => {
-          state.connected = true;
-        },
-        (error) => {
-          // eslint-disable-next-line no-console
-          console.error(error);
-
-          if (this.pgClientState === state) {
-            this.pgClientState = undefined;
-          }
-        },
-      );
-
-      state.client.on('error', (error) => {
-        // eslint-disable-next-line no-console
-        console.error(error);
-
-        if (this.pgClientState === state) {
-          this.pgClientState = undefined;
-        }
-      });
-
-      this.pgClientState = state;
-    }
-
-    if (!this.pgClientState.connected) {
-      await this.pgClientState.connectionPromise;
-    }
-
-    const client = this.pgClientState?.client;
-
-    if (client === undefined) {
-      throw new Error('Database connection failed');
-    }
-
-    return client;
+  private PgClient(): Promise<PgClient> {
+    return this.reconnectablePgClient.PgClient();
   }
 
   async disconnect() {
-    const state = this.pgClientState;
-
-    if (state === undefined) {
-      return;
-    }
-
-    await state.client.end();
-
-    if (this.pgClientState === state) {
-      this.pgClientState = undefined;
-    }
+    await this.reconnectablePgClient.disconnect();
   }
 
   async HourlyStats(from: Date, to: Date): Promise<HourlyStat[]> {
