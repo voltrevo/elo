87f0da2cce858e4bcab2e553b322df3a607d6a30
Andrew Morris <voltrevo@gmail.com>
2022-02-01 14:02:14 +1100

    Convert App to FunctionComponent

--- src/web-frontends/extension/components/App.tsx
+++ src/web-frontends/extension/components/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import TaskQueue from '../../../helpers/TaskQueue';
 import ContentAppClient from '../ContentAppClient';
 import UiState from '../UiState';
 import CollapseIcon from './CollapseIcon';
@@ -11,202 +10,163 @@ type Props = {
   contentApp: ReturnType<typeof ContentAppClient>,
 };
 
-type State = {
-  uiState: UiState;
-
-  collapsed: boolean;
-
-  left?: string;
-  top?: string;
-
-  highlightFillerSound: boolean;
-  highlightFillerWord: boolean;
-
-  dashboardUrl: string;
-};
-
-export default class App extends React.Component<Props, State> {
-  dragData: null | {
+const App: React.FunctionComponent<Props> = ({ contentApp }: Props) => {
+  const dragData = React.useRef<{
     mouseDownPos: { x: number, y: number },
     appPosAtMouseDown: { x: number, y: number },
-  } = null;
+  }>();
 
-  appRef: HTMLDivElement | null = null;
-  dragRef: HTMLDivElement | null = null;
+  const appRef = React.useRef<HTMLDivElement>();
+  const dragRef = React.useRef<HTMLDivElement>();
 
-  windowSize = {
+  const windowSize = React.useRef({
     width: window.innerWidth,
     height: window.innerHeight,
-  };
+  });
 
-  cleanupTasks = new TaskQueue();
+  const [uiState, setUiState] = React.useState(UiState());
 
-  constructor(props: Props) {
-    super(props);
+  const [collapsed, setCollapsed] = React.useState(false);
+  const [highlightFillerSound, setHighlightFillerSound] = React.useState(false);
+  const [highlightFillerWord, setHighlightFillerWord] = React.useState(false);
+  const [left, setLeft] = React.useState<string>();
+  const [top, setTop] = React.useState<string>();
 
-    this.state = {
-      uiState: UiState(),
-      collapsed: false,
-      highlightFillerSound: false,
-      highlightFillerWord: false,
-      dashboardUrl: '#',
-    };
-
-    this.props.contentApp.getDashboardUrl().then(dashboardUrl => {
-      this.setState({ dashboardUrl });
-    });
-  }
-
-  componentWillMount() {
+  React.useEffect(() => {
     let cleanedUp = false;
-
-    this.cleanupTasks.push(() => {
-      cleanedUp = true;
-    });
 
     (async () => {
       while (true) {
-        const newUiState = await this.props.contentApp.getUiState(this.state.uiState.index);
+        const newUiState = await contentApp.getUiState(uiState.index);
 
         if (cleanedUp) {
           break;
         }
 
-        const highlights: Partial<State> = {};
-
-        if (newUiState.fillerSoundBox.count > this.state.uiState.fillerSoundBox.count) {
-          highlights.highlightFillerSound = true;
+        if (newUiState.fillerSoundBox.count > uiState.fillerSoundBox.count) {
+          setHighlightFillerSound(true);
           const count = newUiState.fillerSoundBox.count;
 
           setTimeout(() => {
-            if (this.state.uiState.fillerSoundBox.count === count) {
-              this.setState({
-                highlightFillerSound: false,
-              });
+            if (uiState.fillerSoundBox.count === count) {
+              setHighlightFillerSound(false);
             }
           }, 3000);
         }
 
-        if (newUiState.fillerWordBox.count > this.state.uiState.fillerWordBox.count) {
-          highlights.highlightFillerWord = true;
+        if (newUiState.fillerWordBox.count > uiState.fillerWordBox.count) {
+          setHighlightFillerWord(true);
           const count = newUiState.fillerWordBox.count;
 
           setTimeout(() => {
-            if (this.state.uiState.fillerWordBox.count === count) {
-              this.setState({
-                highlightFillerWord: false,
-              });
+            if (uiState.fillerWordBox.count === count) {
+              setHighlightFillerWord(false);
             }
           }, 3000);
         }
 
-        // TODO: Avoid casting
-        this.setState({ uiState: newUiState, ...highlights } as State);
+        setUiState(newUiState);
       }
     })();
 
-    const onResize = () => {
-      const lastWindowSize = this.windowSize;
+    return () => { cleanedUp = true; };
+  });
 
-      this.windowSize = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
+  const onResize = () => {
+    const lastWindowSize = windowSize.current;
 
-      if (this.dragData !== null || this.appRef === null) {
-        return;
-      }
-
-      const left = this.appRef.style.left;
-      const top = this.appRef.style.top;
-
-      if (!left.endsWith('px') || !top.endsWith('px')) {
-        return;
-      }
-
-      const padding = 18;
-
-      const elementRect = this.appRef.getBoundingClientRect();
-
-      const lastPosRect = {
-        width: lastWindowSize.width - 2 * padding - elementRect.width,
-        height: lastWindowSize.height - 2 * padding - elementRect.height,
-      };
-
-      const posRect = {
-        width: this.windowSize.width - 2 * padding - elementRect.width,
-        height: this.windowSize.height - 2 * padding - elementRect.height,
-      };
-
-      const oldLeft = parseFloat(left) - padding;
-      const oldTop = parseFloat(top) - padding;
-
-      if (oldLeft < 0) {
-        // Do nothing
-      } else if (oldLeft > lastPosRect.width) {
-        this.appRef.style.left = `${padding + oldLeft - lastPosRect.width + posRect.width}`;
-      } else {
-        const xRatio = (parseFloat(left) - padding) / lastPosRect.width;
-        this.appRef.style.left = `${padding + xRatio * posRect.width}px`;
-      }
-
-      if (oldTop < 0) {
-        // Do nothing
-      } else if (oldTop > lastPosRect.height) {
-        this.appRef.style.top = `${padding + oldTop - lastPosRect.height + posRect.height}`;
-      } else {
-        const yRatio = (parseFloat(top) - padding) / lastPosRect.height;
-        this.appRef.style.top = `${padding + yRatio * posRect.height}px`;
-      }
+    windowSize.current = {
+      width: window.innerWidth,
+      height: window.innerHeight,
     };
 
-    window.addEventListener('resize', onResize);
-
-    this.cleanupTasks.push(() => {
-      window.removeEventListener('resize', onResize);
-    });
-  }
-
-  componentWillUnmount() {
-    this.cleanupTasks.run();
-  }
-
-  trySetupDragging() {
-    if (this.appRef === null || this.dragRef === null) {
+    if (dragData.current !== undefined || appRef.current === undefined) {
       return;
     }
 
-    this.dragRef.addEventListener('mousedown', (evt) => {
+    const appLeft = appRef.current.style.left;
+    const appTop = appRef.current.style.top;
+
+    if (!appLeft.endsWith('px') || !appTop.endsWith('px')) {
+      return;
+    }
+
+    const padding = 18;
+
+    const elementRect = appRef.current.getBoundingClientRect();
+
+    const lastPosRect = {
+      width: lastWindowSize.width - 2 * padding - elementRect.width,
+      height: lastWindowSize.height - 2 * padding - elementRect.height,
+    };
+
+    const posRect = {
+      width: windowSize.current.width - 2 * padding - elementRect.width,
+      height: windowSize.current.height - 2 * padding - elementRect.height,
+    };
+
+    const oldLeft = parseFloat(appLeft) - padding;
+    const oldTop = parseFloat(appTop) - padding;
+
+    if (oldLeft < 0) {
+      // Do nothing
+    } else if (oldLeft > lastPosRect.width) {
+      appRef.current.style.left = `${padding + oldLeft - lastPosRect.width + posRect.width}`;
+    } else {
+      const xRatio = (parseFloat(appLeft) - padding) / lastPosRect.width;
+      appRef.current.style.left = `${padding + xRatio * posRect.width}px`;
+    }
+
+    if (oldTop < 0) {
+      // Do nothing
+    } else if (oldTop > lastPosRect.height) {
+      appRef.current.style.top = `${padding + oldTop - lastPosRect.height + posRect.height}`;
+    } else {
+      const yRatio = (parseFloat(appTop) - padding) / lastPosRect.height;
+      appRef.current.style.top = `${padding + yRatio * posRect.height}px`;
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  });
+
+  const trySetupDragging = () => {
+    if (appRef.current === undefined || dragRef.current === undefined) {
+      return;
+    }
+
+    dragRef.current.addEventListener('mousedown', (evt) => {
       if (
         evt.button !== 0 ||
-        this.dragData !== null ||
-        this.appRef === null ||
-        this.dragRef === null
+        dragData.current !== undefined ||
+        appRef.current === undefined ||
+        dragRef.current === undefined
       ) {
         return;
       }
 
-      const appRect = this.appRef.getBoundingClientRect();
+      const appRect = appRef.current.getBoundingClientRect();
 
-      this.dragData = {
+      dragData.current = {
         mouseDownPos: { x: evt.screenX, y: evt.screenY },
         appPosAtMouseDown: { x: appRect.left, y: appRect.top },
       };
 
       const mouseMoveListener = (moveEvt: MouseEvent) => {
-        if (this.dragData === null || this.appRef === null) {
+        if (dragData.current === undefined || appRef.current === undefined) {
           return;
         }
 
         const diffPos = {
-          x: moveEvt.screenX - this.dragData.mouseDownPos.x,
-          y: moveEvt.screenY - this.dragData.mouseDownPos.y,
+          x: moveEvt.screenX - dragData.current.mouseDownPos.x,
+          y: moveEvt.screenY - dragData.current.mouseDownPos.y,
         };
 
-        this.setState({
-          left: `${this.dragData.appPosAtMouseDown.x + diffPos.x}px`,
-          top: `${this.dragData.appPosAtMouseDown.y + diffPos.y}px`,
-        });
+        setLeft(`${dragData.current.appPosAtMouseDown.x + diffPos.x}px`);
+        setTop(`${dragData.current.appPosAtMouseDown.y + diffPos.y}px`);
       };
 
       window.addEventListener('mousemove', mouseMoveListener);
@@ -214,41 +174,47 @@ export default class App extends React.Component<Props, State> {
       const mouseUpListener = () => {
         window.removeEventListener('mousemove', mouseMoveListener);
         window.removeEventListener('mouseup', mouseUpListener);
-        this.dragData = null;
+        dragData.current = undefined;
       };
 
       window.addEventListener('mouseup', mouseUpListener);
     });
-  }
+  };
 
-  render(): React.ReactNode {
-    const { uiState, left = '', top = '' } = this.state;
+  const openDashboard = () => {
+    contentApp.getDashboardUrl().then(dashboardUrl => {
+      const anchorTag = document.createElement('a');
+      anchorTag.href = dashboardUrl;
+      anchorTag.setAttribute('target', '_blank');
+      anchorTag.setAttribute('rel', 'noopener noreferrer');
+      anchorTag.click();
+    });
+  };
 
+  function render(): React.ReactElement {
     const classes = [
       'app',
       ...(uiState.active ? ['active'] : []),
-      ...(this.state.collapsed ? ['collapsed'] : []),
+      ...(collapsed ? ['collapsed'] : []),
     ];
 
     return <div
       className={classes.join(' ')}
       ref={r => {
-        this.appRef = r;
-        this.dragRef = r;
-        this.trySetupDragging();
+        appRef.current = r ?? undefined;
+        dragRef.current = r ?? undefined;
+        trySetupDragging();
       }}
       style={{ left, top }}
     >
-      <div className="app-content">{this.renderAppContent()}</div>
+      <div className="app-content">{renderAppContent()}</div>
     </div>;
   }
 
-  renderAppContent() {
-    const { uiState } = this.state;
-
-    if (this.state.collapsed) {
+  function renderAppContent(): React.ReactElement {
+    if (collapsed) {
       return <div className="body">
-        <ExpandIcon onAction={() => { this.setState({ collapsed: false }); }}/>
+        <ExpandIcon onAction={() => { setCollapsed(false); }}/>
         <div className="center common-centering">
           {uiState.loading
             ? <div className="spinner"></div>
@@ -266,8 +232,8 @@ export default class App extends React.Component<Props, State> {
     }
 
     return <>
-      <CollapseIcon onAction={() => { this.setState({ collapsed: true }); }}/>
-      <PopoutIcon onAction={() => { this.openDashboard(); }}/>
+      <CollapseIcon onAction={() => { setCollapsed(true); }}/>
+      <PopoutIcon onAction={() => { openDashboard(); }}/>
       <div className="body">
         <div className="left spacer">
           <div className="word-box-container spacer">
@@ -275,7 +241,7 @@ export default class App extends React.Component<Props, State> {
               className={[
                 'common-centering',
                 'word-box',
-                ...(this.state.highlightFillerSound ? ['highlight'] : []),
+                ...(highlightFillerSound ? ['highlight'] : []),
               ].join(' ')}
             >
               {uiState.fillerSoundBox.text}
@@ -307,7 +273,7 @@ export default class App extends React.Component<Props, State> {
               className={[
                 'common-centering',
                 'word-box',
-                ...(this.state.highlightFillerWord ? ['highlight'] : []),
+                ...(highlightFillerWord ? ['highlight'] : []),
               ].join(' ')}
             >
               {uiState.fillerWordBox.text}
@@ -318,11 +284,7 @@ export default class App extends React.Component<Props, State> {
     </>;
   }
 
-  openDashboard() {
-    const anchorTag = document.createElement('a');
-    anchorTag.href = this.state.dashboardUrl;
-    anchorTag.setAttribute('target', '_blank');
-    anchorTag.setAttribute('rel', 'noopener noreferrer');
-    anchorTag.click();
-  }
-}
+  return render();
+};
+
+export default App;
