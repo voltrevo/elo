b9fa7a4416fb2d03a1d25d337395793b31439521
Andrew Morris <voltrevo@gmail.com>
2022-02-01 18:45:06 +1100

    Verify code

--- src/web-frontends/extension/components/SignupPage.tsx
+++ src/web-frontends/extension/components/SignupPage.tsx
@@ -15,6 +15,12 @@ const SignUpPage: React.FunctionComponent = () => {
   const [sentEmail, setSentEmail] = React.useState<string>();
   const [verificationCode, setVerificationCode] = React.useState<string>('');
 
+  const [verificationCheck, setVerificationCheck] = React.useState<{
+    email: string,
+    code: string,
+    correct: boolean,
+  }>();
+
   const loginForm = () => <table>
     <tr>
       <td>Email</td>
@@ -89,15 +95,40 @@ const SignUpPage: React.FunctionComponent = () => {
       <td>
         <input
           type="text"
-          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => {
-            setVerificationCode(evt.target.value);
+          onInput={async (evt: React.ChangeEvent<HTMLInputElement>) => {
+            const newCode = evt.target.value;
+            setVerificationCode(newCode);
+
+            if (newCode.length === 6) {
+              const currentEmail = email;
+              const currentCode = newCode;
+              const correct = await appCtx.checkVerificationEmail(email, newCode);
+
+              setVerificationCheck({
+                email: currentEmail,
+                code: currentCode,
+                correct,
+              });
+            }
           }}
           style={{
             backgroundColor: switch_(
               [
                 [verificationCode.length === 0, ''],
                 [verificationCode.length < 6, 'lightyellow'],
-                [verificationCode.length === 6, 'lightblue'],
+                [verificationCode.length === 6, switch_(
+                  [
+                    [
+                      (
+                        verificationCheck?.email !== email ||
+                        verificationCheck?.code !== verificationCode
+                      ),
+                      'lightblue',
+                    ],
+                    [verificationCheck?.correct === true, 'lightgreen'],
+                  ],
+                  'pink',
+                )],
               ],
               'pink',
             ),
