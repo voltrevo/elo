1d52e9ec136819467d73fd5cac560bfdacc4f62e
Andrew Morris <voltrevo@gmail.com>
2022-02-01 18:11:14 +1100

    Add more elements to signup page

--- src/web-frontends/extension/components/SignupPage.tsx
+++ src/web-frontends/extension/components/SignupPage.tsx
@@ -4,28 +4,103 @@ import EloPageContext from '../EloPageContext';
 const SignUpPage: React.FunctionComponent = () => {
   const pageCtx = React.useContext(EloPageContext);
 
+  const [authChoice, setAuthChoice] = React.useState<'signup' | 'login'>('signup');
+  const [email, setEmail] = React.useState('');
+  const [passwd, setPasswd] = React.useState('');
+  const [confirmPasswd, setConfirmPasswd] = React.useState('');
+  const [sentEmail, setSentEmail] = React.useState<string>();
+  const [verificationCode, setVerificationCode] = React.useState<string>('');
+
+  const loginForm = () => <table>
+    <tr>
+      <td>Email</td>
+      <td>
+        <input
+          type="text"
+          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => setEmail(evt.target.value)}
+        />
+      </td>
+    </tr>
+    <tr>
+      <td>Password</td>
+      <td>
+        <input
+          type="password"
+          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => setPasswd(evt.target.value)}
+        />
+      </td>
+    </tr>
+  </table>;
+
+  const signupForm = () => <table>
+    <tr>
+      <td>Email</td>
+      <td>
+        <input
+          type="text"
+          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => setEmail(evt.target.value)}
+        />
+      </td>
+    </tr>
+    <tr>
+      <td>Password</td>
+      <td>
+        <input
+          type="password"
+          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => setPasswd(evt.target.value)}
+        />
+      </td>
+    </tr>
+    <tr>
+      <td>Confirm Password</td>
+      <td>
+        <input
+          type="password"
+          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => {
+            setConfirmPasswd(evt.target.value);
+          }}
+        />
+      </td>
+    </tr>
+    <tr>
+      <td colSpan={2}>
+        <button
+          disabled={!(email && email !== sentEmail && passwd && passwd === confirmPasswd)}
+          onClick={() => setSentEmail(email)}
+        >
+          {email && email === sentEmail ? 'Sent' : 'Send verification email'}
+        </button>
+      </td>
+    </tr>
+    {email && email === sentEmail && <tr>
+      <td colSpan={2}>
+        <button>Resend</button>
+      </td>
+    </tr>}
+    {email && email === sentEmail && <tr>
+      <td>Verification code</td>
+      <td>
+        <input
+          type="text"
+          onInput={(evt: React.ChangeEvent<HTMLInputElement>) => {
+            setVerificationCode(evt.target.value);
+          }}
+        />
+      </td>
+    </tr>}
+  </table>;
+
   return <>
     <h1>Welcome to Elo!</h1>
-    <table>
-      <tr>
-        <td>Email</td>
-        <td>
-          <input type="text" />
-        </td>
-      </tr>
-      <tr>
-        <td>Password</td>
-        <td>
-          <input type="password" />
-        </td>
-      </tr>
-      <tr>
-        <td>Confirm Password</td>
-        <td>
-          <input type="password" />
-        </td>
-      </tr>
-    </table>
+
+    <div>
+      <input type="radio" checked={authChoice === 'signup'} onClick={() => setAuthChoice('signup')} /> Sign Up
+    </div>
+    <div>
+      <input type="radio" checked={authChoice === 'login'} onClick={() => setAuthChoice('login')} /> Log In
+    </div>
+
+    {authChoice === 'login' ? loginForm() : signupForm()}
   </>;
 };
 
