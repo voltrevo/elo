2ec8557db6abf1a69310674eb5c0b4d5286f60da
Andrew Morris <voltrevo@gmail.com>
2022-02-04 15:04:45 +1100

    wip Dialog

--- src/web-frontends/extension/components/Dialog.tsx
+++ src/web-frontends/extension/components/Dialog.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
 
-const Dialog: React.FunctionComponent = () => {
-  return <>Dialog</>;
+import { Modal } from '@mui/material';
+import EloPageContext from '../EloPageContext';
+
+const Dialog: React.FunctionComponent = ({ children }) => {
+  const pageCtx = React.useContext(EloPageContext);
+
+  return <Modal
+    open={true}
+    onClose={() => { pageCtx.update({ dialog: '' }); }}
+  >
+    <div style={{
+      outline: 0,
+      backgroundColor: 'white',
+    }}>{children}</div>
+  </Modal>;
 };
 
 export default Dialog;
