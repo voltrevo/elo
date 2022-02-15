afa96136e97531979ef8183f2dff6fceb12dbb5c
Andrew Morris <voltrevo@gmail.com>
2022-02-15 14:49:02 +1100

    Send hello msg on slack

--- projects/elo-slack-automation/src/sayHello.ts
+++ projects/elo-slack-automation/src/sayHello.ts
@@ -1,3 +1,15 @@
-export default function sayHello() {
-  console.log('Hello world!');
+import * as slack from 'slack';
+
+const config = require('../../config.json');
+
+const slackToken: string = config.slackToken;
+
+export default async function sayHello() {
+  const resp = await slack.chat.postMessage({
+    token: slackToken,
+    channel: 'automation-testing',
+    text: 'Hello!',
+  });
+
+  console.log('done', resp);
 }
