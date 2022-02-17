903fbfa1fbbf44d80984894d143067615a049ec8
Andrew Morris <voltrevo@gmail.com>
2022-02-17 03:45:15 +0000

    Stop serving static files

--- src/routes/defineRoutes.ts
+++ src/routes/defineRoutes.ts
@@ -1,11 +1,7 @@
 /* eslint-disable no-console */
 
-import path from 'path';
-
 import route from 'koa-route';
-import serveStaticCache from 'koa-static-cache';
 
-import dirs from '../dirs';
 import { generateUserId } from '../userIds';
 import AppComponents from '../AppComponents';
 import defineAnalyze from './defineAnalyze';
@@ -13,12 +9,6 @@ import defineStartSession from './defineStartSession';
 
 export default function defineRoutes(appComponents: AppComponents) {
   const { koaApp } = appComponents;
-
-  koaApp.use(serveStaticCache(path.join(dirs.build), {
-    alias: {
-      '/': '/index.html',
-    },
-  }));
 
   defineAnalyze(appComponents);
   defineStartSession(appComponents);
