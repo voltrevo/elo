384159604c3777f276b7cc3928da7b5136a950d9
Andrew Morris <voltrevo@gmail.com>
2022-02-17 22:46:17 +0000

    Start rendering actual elo page

--- projects/elo-page/src/elo-page/index.tsx
+++ projects/elo-page/src/elo-page/index.tsx
@@ -1,19 +1,25 @@
 import * as ReactDOM from 'react-dom';
 
-import App from './components/App';
-
-import PageContext, { initPageContext } from './PageContext';
+import EloPage from './components/EloPage';
+import { makeLocalContentAppClient } from './ContentAppClient';
+import ContentAppContext from './ContentAppContext';
+import EloPageContext, { initEloPageContext } from './EloPageContext';
+import IRawStorage from './storage/IRawStorage';
+import Storage from './storage/Storage';
 
 window.addEventListener('load', async () => {
-  const pageCtx = initPageContext();
-
   const appDiv = document.createElement('div');
   document.body.appendChild(appDiv);
 
+  const eloClient = makeLocalContentAppClient({} as any);
+  const pageCtx = initEloPageContext(new Storage({} as IRawStorage, 'elo'));
+
   ReactDOM.render(
-    <PageContext.Provider value={pageCtx}>
-      <App/>
-    </PageContext.Provider>,
+    <ContentAppContext.Provider value={eloClient}>
+      <EloPageContext.Provider value={pageCtx}>
+        <EloPage/>
+      </EloPageContext.Provider>
+    </ContentAppContext.Provider>,
     appDiv,
   );
 });
