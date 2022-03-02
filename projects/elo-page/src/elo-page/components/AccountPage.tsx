42bd2a91e54b41c77a868f16b785ff784ea9380d
Andrew Morris <voltrevo@gmail.com>
2022-03-02 03:11:52 +0000

    Update AccountPage

--- projects/elo-page/src/elo-page/components/AccountPage.tsx
+++ projects/elo-page/src/elo-page/components/AccountPage.tsx
@@ -5,36 +5,38 @@ import ContentAppContext from '../ContentAppContext';
 import EloPageContext from '../EloPageContext';
 import AsyncButton from './AsyncButton';
 import AsyncContent from './AsyncContent';
+import Field from './Field';
 import Page from './Page';
+import Section from './Section';
 
 const AccountPage: React.FunctionComponent = () => {
   const appCtx = React.useContext(ContentAppContext);
   const pageCtx = React.useContext(EloPageContext);
 
   return <Page classes={['form-page']}>
-    <h1>Account</h1>
+    <Section>
+      <h1>Account</h1>
+      <Field>
+        <div>Email</div>
+        <div>
+          <AsyncContent load={() => appCtx.getEmail()}/>
+        </div>
+      </Field>
+      <div className="button-column">
+        <AsyncButton
+          onClick={async () => {
+            await appCtx.logout();
+            pageCtx.update({ needsAuth: true });
 
-    <div className="field">
-      <div>Email</div>
-      <div>
-        <AsyncContent load={() => appCtx.getEmail()}/>
+            delay(250).then(() => {
+              pageCtx.update({ page: 'WelcomePage' });
+            });
+          }}
+        >
+          Log Out
+        </AsyncButton>
       </div>
-    </div>
-
-    <div className="button-column">
-      <AsyncButton
-        onClick={async () => {
-          await appCtx.logout();
-          pageCtx.update({ needsAuth: true });
-
-          delay(250).then(() => {
-            pageCtx.update({ page: 'WelcomePage' });
-          });
-        }}
-      >
-        Log Out
-      </AsyncButton>
-    </div>
+    </Section>
   </Page>;
 };
 
