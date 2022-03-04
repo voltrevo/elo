753189f6d4c4628807ebe0b68cf6f360db652f09
Andrew Morris <voltrevo@gmail.com>
2022-03-04 02:42:56 +0000

    Remove unnecessary wrapper

--- projects/database/src/database/DbClient.ts
+++ projects/database/src/database/DbClient.ts
@@ -1,20 +1,74 @@
-/* eslint-disable camelcase */
-
 import PgClient from './PgClient';
-import ReconnectablePgClient from './ReconnectablePgClient';
 
 export default class DbClient {
-  reconnectablePgClient: ReconnectablePgClient;
+  private pgClientState?: {
+    client: PgClient;
+    connected: boolean;
+    connectionPromise: Promise<void>;
+  };
 
-  constructor(pgConnString: string) {
-    this.reconnectablePgClient = new ReconnectablePgClient(pgConnString);
-  }
+  constructor(private pgConnString: string) {}
 
-  PgClient(): Promise<PgClient> {
-    return this.reconnectablePgClient.PgClient();
+  async PgClient(): Promise<PgClient> {
+    if (this.pgClientState === undefined) {
+      const client = new PgClient(this.pgConnString);
+
+      const state: DbClient['pgClientState'] = {
+        client,
+        connected: false,
+        connectionPromise: client.connect(),
+      };
+
+      state.connectionPromise.then(
+        () => {
+          state.connected = true;
+        },
+        (error) => {
+          // eslint-disable-next-line no-console
+          console.error(error);
+
+          if (this.pgClientState === state) {
+            this.pgClientState = undefined;
+          }
+        },
+      );
+
+      state.client.on('error', (error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+
+        if (this.pgClientState === state) {
+          this.pgClientState = undefined;
+        }
+      });
+
+      this.pgClientState = state;
+    }
+
+    if (!this.pgClientState.connected) {
+      await this.pgClientState.connectionPromise;
+    }
+
+    const client = this.pgClientState?.client;
+
+    if (client === undefined) {
+      throw new Error('Database connection failed');
+    }
+
+    return client;
   }
 
   async disconnect() {
-    await this.reconnectablePgClient.disconnect();
+    const state = this.pgClientState;
+
+    if (state === undefined) {
+      return;
+    }
+
+    await state.client.end();
+
+    if (this.pgClientState === state) {
+      this.pgClientState = undefined;
+    }
   }
 }
