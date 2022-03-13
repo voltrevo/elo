038995373a5db3dae997c468afb00e0f5db457b1
Andrew Morris <voltrevo@gmail.com>
2022-03-13 23:49:28 +0000

    Apply defineRegister

--- projects/elo-worker/src/elo-worker/routes/defineRoutes.ts
+++ projects/elo-worker/src/elo-worker/routes/defineRoutes.ts
@@ -6,12 +6,14 @@ import { generateUserId } from '../userIds';
 import AppComponents from '../AppComponents';
 import defineAnalyze from './defineAnalyze';
 import defineStartSession from './defineStartSession';
+import defineRegister from './defineRegister';
 
 export default function defineRoutes(appComponents: AppComponents) {
   const { koaApp } = appComponents;
 
   defineAnalyze(appComponents);
   defineStartSession(appComponents);
+  defineRegister(appComponents);
 
   koaApp.use(route.post('/generateId', async ctx => {
     ctx.body = generateUserId();
