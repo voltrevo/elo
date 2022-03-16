fc3f95409a10bd94b09c2c7671a10382cec4c93a
Andrew Morris <voltrevo@gmail.com>
2022-03-16 03:06:37 +0000

    Log posts to /zoom-webhook-test

--- projects/zoom-backend/src/zoom-backend/defineRoutes.ts
+++ projects/zoom-backend/src/zoom-backend/defineRoutes.ts
@@ -1,7 +1,19 @@
 /* eslint-disable no-console */
 
+import route from 'koa-route';
+
 import AppComponents from './AppComponents';
 
 export default function defineRoutes(appComponents: AppComponents) {
-  // TODO
+  const { koaApp } = appComponents;
+
+  koaApp.use(route.post('/zoom-webhook-test', async ctx => {
+    console.log('POST /zoom-webhook-test', {
+      ip: ctx.ip,
+      headers: ctx.request.headers,
+      body: ctx.request.body,
+    });
+
+    ctx.status = 200;
+  }));
 }
