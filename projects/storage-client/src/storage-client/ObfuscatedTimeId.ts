384ed240cdf22af09999672ababaada9b84d4ac2
Andrew Morris <voltrevo@gmail.com>
2022-04-13 06:34:45 +0000

    StorageTimedCollection

--- projects/storage-client/src/storage-client/ObfuscatedTimeId.ts
+++ projects/storage-client/src/storage-client/ObfuscatedTimeId.ts
@@ -3,14 +3,18 @@ import { keccak256 } from "js-sha3";
 import obfuscateTime from "./obfuscateTime";
 import Range from "../common-pure/Range";
 
-export default function ObfuscatedTimeId(seed: string, t: number) {
-  const alphabet = 'abcdefghijklmnopqrstuvwzyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+export default function ObfuscatedTimeId(
+  seed: string,
+  t: number,
+  trailValue = BigInt(`0x${keccak256(Range(5).map(() => Math.random()).join(''))}`),
+) {
+  const alphabet = '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwzyz';
 
   const expander = 2n ** 139n;
 
   let ot = BigInt(Math.floor(obfuscateTime(seed, t)));
   ot *= expander;
-  ot += BigInt(`0x${keccak256(Range(5).map(() => Math.random()).join(''))}`) % expander;
+  ot += trailValue % expander;
 
   let result = '';
 
--- projects/storage-client/src/storage-client/StorageClient.ts
+++ projects/storage-client/src/storage-client/StorageClient.ts
@@ -7,6 +7,8 @@ import { decryptWithKeyHash, encryptWithKeyHash, getKeyHash } from './encryption
 import StorageKeyCalculator from './StorageKeyCalculator';
 import decode from '../elo-types/decode';
 import buffersEqual from '../common-pure/buffersEqual';
+import base58 from '../common-pure/base58';
+import ObfuscatedTimeId from './ObfuscatedTimeId';
 
 export default class StorageClient {
   constructor(
@@ -22,7 +24,11 @@ export default class StorageClient {
     return new StorageCollection(this, elementType, collectionId);
   }
 
-  async fullGet<T extends io.Mixed>(type: T, collectionId: string, elementId: string): Promise<T | nil> {
+  TimedCollection<T extends io.Mixed>(elementType: T, collectionId: string) {
+    return new StorageTimedCollection(this.Collection(elementType, collectionId));
+  }
+
+  async fullGet<T extends io.Mixed>(type: T, collectionId: string, elementId: string): Promise<io.TypeOf<T> | nil> {
     const encryptedBuf = await this.rpcClient.get(collectionId, elementId);
 
     if (encryptedBuf === nil) {
@@ -49,7 +55,7 @@ export default class StorageClient {
     collectionId: string,
     minElementId: string,
     maxElementId: string,
-  ): Promise<[string, T][]> {
+  ): Promise<[string, io.TypeOf<T>][]> {
     const encryptedResults = await this.rpcClient.getRange(collectionId, minElementId, maxElementId);
 
     let results: [string, T][] = [];
@@ -73,7 +79,7 @@ export default class StorageClient {
     return results;
   }
 
-  async fullSet<T extends io.Mixed>(_type: T, collectionId: string, elementId: string, value: T | nil) {
+  async fullSet<T extends io.Mixed>(_type: T, collectionId: string, elementId: string, value: io.TypeOf<T> | nil) {
     if (value === nil) {
       await this.rpcClient.set(collectionId, elementId, nil);
       return;
@@ -92,12 +98,12 @@ export class StorageElement<T extends io.Mixed> {
     public elementId: string,
   ) {}
 
-  async get(): Promise<T | nil> {
+  async get(): Promise<io.TypeOf<T> | nil> {
     const value = await this.client.fullGet(this.type, this.collectionId, this.elementId);
     return value;
   }
 
-  async set(value: T | nil): Promise<void> {
+  async set(value: io.TypeOf<T> | nil): Promise<void> {
     await this.client.fullSet(this.type, this.collectionId, this.elementId, value);
   }
 }
@@ -113,7 +119,7 @@ export class StorageCollection<T extends io.Mixed> {
     return new StorageElement(this.client, this.elementType, this.collectionId, elementId);
   }
 
-  async RangeEntries(minElementId: string, maxElementId: string): Promise<[string, T][]> {
+  async RangeEntries(minElementId: string, maxElementId: string): Promise<[string, io.TypeOf<T>][]> {
     return await this.client.fullGetRange(
       this.elementType,
       this.collectionId,
@@ -122,8 +128,52 @@ export class StorageCollection<T extends io.Mixed> {
     );
   }
 
-  async Range(minElementId: string, maxElementId: string): Promise<T[]> {
+  async Range(minElementId: string, maxElementId: string): Promise<io.TypeOf<T>[]> {
     const entries = await this.RangeEntries(minElementId, maxElementId);
     return entries.map(([, element]) => element);
+  }
+}
+
+export class StorageTimedCollection<T extends io.Mixed> {
+  _obfuscationSeed?: string;
+
+  constructor(public collection: StorageCollection<T>) {}
+
+  Element(elementId: string) {
+    return this.collection.Element(elementId);
+  }
+
+  async RangeEntries(minTime: number, maxTime: number) {
+    const obfuscationSeed = await this.ObfuscationSeed();
+
+    return await this.collection.RangeEntries(
+      ObfuscatedTimeId(obfuscationSeed, minTime, 0n),
+      ObfuscatedTimeId(obfuscationSeed, maxTime, 0n),
+    );
+  }
+
+  async Range(minTime: number, maxTime: number) {
+    const entries = await this.RangeEntries(minTime, maxTime);
+    return entries.map(([, element]) => element);
+  }
+
+  private async ObfuscationSeed() {
+    if (this._obfuscationSeed === nil) {
+      const seedElement = this.collection.client.Element(
+        io.string,
+        `${this.collection.collectionId}:obfuscationSeed`,
+      );
+  
+      let obfuscationSeed = await seedElement.get();
+  
+      if (obfuscationSeed === nil) {
+        obfuscationSeed = base58.encode(crypto.getRandomValues(new Uint8Array(32)));
+        await seedElement.set(obfuscationSeed);
+      }
+  
+      this._obfuscationSeed = obfuscationSeed;
+    }
+
+    return this._obfuscationSeed;
   }
 }
