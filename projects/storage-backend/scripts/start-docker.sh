5340c650aeea5b45c29402f5eab79d42c9297b62
Andrew Morris <voltrevo@gmail.com>
2022-04-28 04:23:17 +0000

    Fix port in start-docker.sh

--- projects/storage-backend/scripts/start-docker.sh
+++ projects/storage-backend/scripts/start-docker.sh
@@ -9,7 +9,7 @@ PROJECT="storage-backend"
 docker run \
   --name "$PROJECT"-"$TAG" \
   -d \
-  -p"$PORT":53047 \
+  -p"$PORT":39661 \
   --mount type=bind,source="$HOME"/elo-config/"$PROJECT"-"$TAG".json,target=/app/config.json,readonly \
   --restart=unless-stopped \
   "$PROJECT":"$TAG"
