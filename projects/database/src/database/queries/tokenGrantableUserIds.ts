ead68f8598a4b32f00986ab81114778f8281f36d
Andrew Morris <voltrevo@gmail.com>
2022-05-23 06:27:25 +0000

    Add queries for tokenGrantableUserIds

---
+++ projects/database/src/database/queries/tokenGrantableUserIds.ts
@@ -0,0 +1,63 @@
+import * as io from 'io-ts';
+import { Client as PgClient } from 'pg';
+
+import nil from "../../common-pure/nil";
+import decode from '../../elo-types/decode';
+import Database from '../Database';
+
+const TokenGrantableUserId = io.type({
+  userId: io.string,
+  granted: io.boolean,
+});
+
+const tokenGrantableUserIds = {
+  lookup: async (
+    dbOrClient: Database | PgClient,
+    userId: string,
+  ) => {
+    const pgClient = dbOrClient instanceof Database
+      ? await dbOrClient.PgClient()
+      : dbOrClient;
+
+    const res = await pgClient.query(
+      `
+        SELECT data FROM token_grantable_user_ids
+        WHERE
+          user_id = $1
+      `,
+      [userId],
+    );
+
+    const row = res.rows[0];
+
+    if (row === nil) {
+      return nil;
+    }
+
+    return decode(TokenGrantableUserId, row);
+  },
+
+  setGranted: async (
+    dbOrClient: Database | PgClient,
+    userId: string,
+    granted: boolean,
+  ) => {
+    const pgClient = dbOrClient instanceof Database
+      ? await dbOrClient.PgClient()
+      : dbOrClient;
+    
+    await pgClient.query(
+      `
+        UPDATE token_grantable_user_ids
+        SET granted = $2
+        WHERE user_id = $1
+      `,
+      [
+        userId,
+        granted,
+      ],
+    );
+  },
+};
+
+export default tokenGrantableUserIds;
