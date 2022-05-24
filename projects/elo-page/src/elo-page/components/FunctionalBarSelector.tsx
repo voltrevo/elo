ab79e6bd8cd9e32968b37432ad888157555ba1db
Andrew Morris <voltrevo@gmail.com>
2022-05-24 13:31:46 +1000

    Add loading states to FunctionalBarSelector

--- projects/elo-page/src/elo-page/components/FunctionalBarSelector.tsx
+++ projects/elo-page/src/elo-page/components/FunctionalBarSelector.tsx
@@ -1,3 +1,4 @@
+import { CircleNotch, XCircle } from 'phosphor-react';
 import * as React from 'react';
 
 type Props = {
@@ -5,7 +6,7 @@ type Props = {
   options: string[];
   displayMap?: Record<string, string>;
   selection?: string;
-  onSelect?: (selection: string | undefined) => void;
+  onSelect?: (selection: string | undefined) => unknown;
 };
 
 const FunctionalBarSelector: React.FunctionComponent<Props> = (
@@ -17,18 +18,58 @@ const FunctionalBarSelector: React.FunctionComponent<Props> = (
     onSelect = () => {},
   }: Props,
 ) => {
-  return <div className={['bar-selector', ...classes].join(' ')}>
-    {options.map(option => (
-      <div
-        className={`cell ${option === selection && 'selected'}`}
-        onClick={() => onSelect(option === selection
-            ? undefined
-            : option,
-        )}
-      >
-        {displayMap[option] ?? option}
+  let [progressState, setProgressState] = React.useState<'rest' | 'loading' | Error>('rest');
+  let [loadingStartedTime, setLoadingStartedTime] = React.useState(0);
+  let [lastUpdateTime, setLastUpdateTime] = React.useState(0);
+
+  return <div className="bar-selector-wrapper">
+    <div className={['bar-selector', ...classes].join(' ')}>
+      {options.map(option => (
+        <div
+          className={`cell ${option === selection && 'selected'}`}
+          onClick={async () => {
+            if (progressState === 'loading') {
+              console.warn('Ignoring click while loading');
+              return;
+            }
+
+            const clickTime = Date.now();
+
+            setTimeout(() => {
+              setLoadingStartedTime(clickTime);
+            }, 200);
+
+            try {
+              setProgressState('loading');
+
+              await onSelect(option === selection
+                ? undefined
+                : option,
+              );
+
+              setProgressState('rest');
+            } catch (error) {
+              setProgressState(error as Error);
+            } finally {
+              setLastUpdateTime(Date.now());
+            }
+          }}
+        >
+          {displayMap[option] ?? option}
+        </div>
+      ))}
+    </div>
+    {progressState === 'loading' && loadingStartedTime > lastUpdateTime && <div className="progress-state spinner">
+      <CircleNotch size={24} />
+    </div>}
+    {progressState instanceof Error && <div className="progress-state">
+      <XCircle size={24} />
+      <div className="bar-error-wrapper">
+        <div className="bar-error">
+          {progressState.message}
+        </div>
       </div>
-    ))}
+    </div>}
   </div>;
 };
 
