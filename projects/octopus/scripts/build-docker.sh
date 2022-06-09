414b356108ee4b8dd985aabb8e0b1fa0269b2e63
Andrew Morris <voltrevo@gmail.com>
2022-06-09 00:18:37 +0000

    Add meta dir

--- projects/octopus/scripts/build-docker.sh
+++ projects/octopus/scripts/build-docker.sh
@@ -19,4 +19,9 @@ rm -f build/"$PROJECT"-"$TAG".tar*
 docker save "$PROJECT":"$TAG" --output build/"$PROJECT"-"$TAG".tar
 gzip build/"$PROJECT"-"$TAG".tar
 
+META_DIR=build/meta-"$PROJECT"-"$TAG"
+mkdir "$META_DIR"
+echo "$TAG" >"$META_DIR"/TAG
+echo "$PROJECT" >"$META_DIR"/PROJECT
+
 echo Success build/"$PROJECT"-"$TAG".tar.gz
