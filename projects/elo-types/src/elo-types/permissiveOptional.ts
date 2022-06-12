7db986866103ba7cc98eaccc92b03fc35294298c
Andrew Morris <voltrevo@gmail.com>
2022-06-12 18:06:11 +1000

    Improve type.name for permissiveOptional

--- projects/elo-types/src/elo-types/permissiveOptional.ts
+++ projects/elo-types/src/elo-types/permissiveOptional.ts
@@ -8,7 +8,7 @@ import nil from '../common-pure/nil';
  */
 export default function permissiveOptional<T extends io.Mixed>(type: T) {
   return new io.Type<io.TypeOf<T> | nil, io.TypeOf<T> | nil | null, unknown>(
-    'permissiveOptional',
+    `permissiveOptional(${type.name})`,
     (u): u is io.TypeOf<T> | nil => type.is(u) || u === nil,
     (u, c) => {
       if (u === null || u === nil) {
