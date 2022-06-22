5d178d30a6da4fa34f3a4a3a82bc431e375d9a86
Andrew Morris <voltrevo@gmail.com>
2022-06-22 11:52:56 +1000

    octopus-lite

---
+++ projects/octopus/scripts/build-docker-lite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+PROJECT="octopus-lite"
+TAG="git-$(git rev-parse HEAD | head -c7)"
+
+docker build . -t "$PROJECT":"$TAG"
+rm -f build/"$PROJECT"-"$TAG".tar*
+docker save "$PROJECT":"$TAG" --output build/"$PROJECT"-"$TAG".tar
+gzip build/"$PROJECT"-"$TAG".tar
+
+META_DIR=build/meta-"$PROJECT"-"$TAG"
+mkdir "$META_DIR"
+echo "$TAG" >"$META_DIR"/TAG
+echo "$PROJECT" >"$META_DIR"/PROJECT
+
+echo Success build/"$PROJECT"-"$TAG".tar.gz
---
+++ projects/octopus/scripts/start-docker-lite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+PROJECT="octopus-lite"
+# TAG=git-eff1a71-model-35d4164
+
+docker run \
+  --name "$PROJECT"-"$TAG" \
+  -d \
+  --net=host \
+  --mount type=bind,source="$HOME"/elo-config/"$PROJECT"-"$TAG".json,target=/app/config.json,readonly \
+  --restart=unless-stopped \
+  "$PROJECT":"$TAG"
